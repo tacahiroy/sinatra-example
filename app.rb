a71bb0e59d1827baf03f19977b7d050ffd08ee53
tacahiroy <tacahiroy@gmail.com>
2012-01-21 01:05:06 +0000

    a few change

--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
 # coding: utf-8 
 require "bundler/setup"
 
-configure do
-  set :app_file, __FILE__
-  set :server, %w[thin mongrel webrick]
-
-  enable :sessions
-end
-
-helpers do
-  def hello(word)
-    "Hello #{word}"
-  end
-end
-
-
 class Author < ActiveRecord::Base
   has_many :books
 end
@@ -24,15 +10,35 @@ class Book < ActiveRecord::Base
 end
 
 
+configure :development do
+  enable :sessions, :clean_trace
+  disable :logging, :dump_errors
+end
+
+configure do
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set :server, %w[thin mongrel webrick]
+  set :slim, { :format => :html5 }
+
+  enable :static, :sessions
+end
+
+helpers do
+  def hello(word)
+    "Hello #{word}"
+  end
+end
+
 ##
 # routing
 #
 get "/" do
-  slim :index, :format => :html
+  slim :index
 end
 
 post "/greeting" do
-  slim :greeting, :format => :html
+  slim :greeting
 end
 
 __END__
